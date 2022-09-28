75ce31a01e630a38358673941d2cd9a8c6631586
Leonora Mae Schipper <elliemaegalyean@gmail.com>
2022-09-28 18:22:24 -0400

    Made extension match regardless of case

--- FileOrganiser/modes/extensions.py
+++ FileOrganiser/modes/extensions.py
@@ -2,4 +2,4 @@ import os
 
 def make_mover(folder, rules):
     return lambda x: os.rename(x, os.path.join(folder, os.path.basename(x))
-                               ) if os.path.splitext(x)[-1] in rules else False
+                               ) if os.path.splitext(x)[-1].lower() in rules else False
